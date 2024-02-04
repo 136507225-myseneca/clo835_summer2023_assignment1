75dd3a4ff6462200f28b025b4f7e3d2f3cb57d18
Tobe Segun <Tobe.Segun@wolterskluwer.com>
2024-02-03 23:32:21 -0500

    removed terraform

--- terrafrom/modules/EC2/output.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip_my_instance" {
-  value = aws_instance.my_instance.public_ip
-}
--- terrafrom/modules/SG/output.tf
+++
@@ -1,4 +0,0 @@
-output "web_sg_id" {
-  description = "The ID of the AWS security group created for web servers"
-  value       = aws_security_group.web_sg.id
-}
